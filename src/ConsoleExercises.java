68030c5245c563c1eb0ee90761553fd0ac232d68
David Renteria <david.j.renteria1@gmail.com>
2023-03-24 16:59:04 -0500

    still working

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("The value of pi is approximately %.2f", pi);
+
+        System.out.print("Enter highest score on a test: ");
+        int highScore = scanner.nextInt();
+        System.out.println("Your highest score was " + highScore);
+
+    }
+}
