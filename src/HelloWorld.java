67ee15633db3982fc8c6e3bc9529da860e1af2f5
David Renteria <david.j.renteria1@gmail.com>
2023-03-24 16:16:57 -0500

    forgot to add/commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,56 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+        System.out.println();
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "TGIF!!";
+        System.out.println(myString);
+
+//        myString = 'd';
+//        myString = 3.14159;
+
+        long myNumber;
+//        myNumber = 3.14;
+        myNumber = 123;
+//        float myNumber = 3.14;
+//        myNumber = (float) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        System.out.println( x += 5);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println( y += x);
+
+        int x = 10;
+        int y = 2;
+        System.out.println( x /= y);
+        System.out.println( y -= x);
+
+
+
+
+
+
+    }
+
+}
