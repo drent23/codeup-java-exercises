f3bb947d143e2f5c5e843073d03b9537aab09ba4
David Renteria <david.j.renteria1@gmail.com>
2023-03-27 15:56:16 -0500

    string basics done

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -6,5 +6,9 @@ public class StringExercise {
         String thoughtCtrl = "We don't need no though control";
         System.out.println(noEd);
         System.out.println(thoughtCtrl);
+        String checkIt = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkIt);
+        String backSlash = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
     }
 }
