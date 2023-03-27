750d20f340f3ae9d94cb61e10784cf7953f7f60e
David Renteria <david.j.renteria1@gmail.com>
2023-03-27 16:02:33 -0500

    will finish later tonight

---
+++ src/Bob.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String [] args) {
+        Scanner scannerObj = new Scanner(System.in);
+        System.out.println("Hello, I'm Bob.  Feel free to communicate with me however you'd like");
+        String userRes = scannerObj.nextLine();
+    }
+}
