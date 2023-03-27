f964c875c6c14c6654319f838d713263f4ebfb8e
David Renteria <david.j.renteria1@gmail.com>
2023-03-27 12:37:42 -0500

    fizzbuzz done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,31 +2,17 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
-        do {
-            System.out.println("i = " + i);
-            i += 2;
-        } while (i <= 100);
-
-        int j = 100;
-        do {
-            System.out.println("j = " + j);
-            j -= 5;
-        } while (j >= -10);
-
-        long k = 2;
-        do {
-            System.out.println("k = " + k);
-            k *= k;
-        } while (k < 1000000);
-
-        for(int m = 100; m >= -10; m -= 5) {
-            System.out.println("m = " + m);
+        for(int i = 1; i <= 100; i++) {
+            if(i % 3 == 0 & i % 5 != 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 & i % 3 != 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0 & i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else
+            System.out.println(i);
         }
 
-        for(long n = 2; n < 1000000; n *= n) {
-            System.out.println("n = " + n);
-        }
     }
 
     }
