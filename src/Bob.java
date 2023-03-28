912d78a0582a764fc291c156bd23c0c7f117f7f9
David Renteria <david.j.renteria1@gmail.com>
2023-03-28 08:52:00 -0500

    Bob done, I think

--- src/Bob.java
+++ src/Bob.java
@@ -5,5 +5,14 @@ public class Bob {
         Scanner scannerObj = new Scanner(System.in);
         System.out.println("Hello, I'm Bob.  Feel free to communicate with me however you'd like");
         String userRes = scannerObj.nextLine();
+        if(userRes.lastIndexOf() == "?") {
+            System.out.println("Sure");
+        } else if(userRes.lastIndexOf() == "!") {
+            System.out.println("Whoa chill out!");
+        } else if(userRes.lastIndexOf() == "") {
+            System.out.println("Fine. Be that way!");
+        } else() {
+            System.out.println("Whatever.");
+        }
     }
 }
