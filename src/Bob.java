6b892ec4cdfc235bc87f953c1f6a298bddbedf92
David Renteria <david.j.renteria1@gmail.com>
2023-03-28 09:36:53 -0500

    Bob done correctly now

--- src/Bob.java
+++ src/Bob.java
@@ -5,14 +5,14 @@ public class Bob {
         Scanner scannerObj = new Scanner(System.in);
         System.out.println("Hello, I'm Bob.  Feel free to communicate with me however you'd like");
         String userRes = scannerObj.nextLine();
-        if(userRes.lastIndexOf() == "?") {
-            System.out.println("Sure");
-        } else if(userRes.lastIndexOf() == "!") {
-            System.out.println("Whoa chill out!");
-        } else if(userRes.lastIndexOf() == "") {
-            System.out.println("Fine. Be that way!");
-        } else() {
-            System.out.println("Whatever.");
-        }
+        if(userRes.endsWith("?")) {
+                System.out.println("Sure");
+            } else if(userRes.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if(userRes == "") {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
     }
 }
